9ce2890114ea53b4b54da2c14438a04cb04f3f45
nadziraraa <nadziraazahra10@gamil.com>
2021-11-13 22:35:48 +0700

    Add project files.

---
+++ ServerConfigForm-P5-20190140127/Form1.cs
@@ -0,0 +1,43 @@
+﻿using ServiceMtk_P1_20190140127;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_20190140127
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelOnOrOff.Text = "Server ON";
+            labelKet.Text = "Klik OFF untuk Mematikan Server";
+            Host.Open();
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            Host.Close();
+            labelKet.Text = "Klik ON untuk Menjalankan Server";
+            labelOnOrOff.Text = "Server OFF";
+        }
+    }
+}
---
+++ ServerConfigForm-P5-20190140127/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20190140127;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_20190140127
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+
+    }
+}
